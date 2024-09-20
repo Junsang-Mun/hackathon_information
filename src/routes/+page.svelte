255f4312dc012376393546bc2e62044710939396
Junsang Mun <mun.js@aol.com>
2024-09-20 21:09:02 +0900

    feat: timetable updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,111 @@
-<h1>Home</h1>
+<script>
+	import { Grid, Row, Column } from "carbon-components-svelte";
+</script>
+
+<h1>Welcome!</h1>
+<hr/>
+<h3>Day 1</h3>
+<br/>
+<Grid>
+	<Row>
+		<Column><h5>Time</h5></Column>
+		<Column><h5>Highlights</h5></Column>
+	</Row>
+	<Row>
+		<Column><p>11:30~13:00</p></Column>
+		<Column><p>Registration</p></Column>
+	</Row>
+	<Row>
+		<Column><p>13:00~14:00</p></Column>
+		<Column><p>Opening remarks & Orientation</p></Column>
+	</Row>
+	<Row>
+		<Column><p>14:00~15:00</p></Column>
+		<Column><p>Keynote speech & Publish the hackathon challenges</p></Column>
+	</Row>
+	<Row>
+		<Column><p>15:00~</p></Column>
+		<Column><p>1<sup>st</sup> hackathon</p></Column>
+	</Row>
+</Grid>
+<br/>
+<h3>Day 2</h3>
+<br/>
+<Grid>
+	<Row>
+		<Column><p>All day</p></Column>
+		<Column><p>1<sup>st</sup> hackathon</p></Column>
+	</Row>
+</Grid>
+<br/>
+<h3>Day 3</h3>
+<br/>
+<Grid>
+	<Row>
+		<Column><p>~09:00</p></Column>
+		<Column><p>1<sup>st</sup> hackathon</p></Column>
+	</Row>
+	<Row>
+		<Column><p>09:00~11:00</p></Column>
+		<Column><p>Hackathon presentation</p></Column>
+	</Row>
+	<Row>
+		<Column><p>11:00~11:30</p></Column>
+		<Column><p>Judging process</p></Column>
+	</Row>
+	<Row>
+		<Column><p>11:30~12:00</p></Column>
+		<Column><p>Awards ceremony & lunch</p></Column>
+	</Row>
+	<Row>
+		<Column><p>13:00~15:00</p></Column>
+		<Column><p>Gwanghwamun Tour</p></Column>
+	</Row>
+</Grid>
+<br/>
+<h3>Day 4</h3>
+<br/>
+<Grid>
+	<Row>
+		<Column><p>13:30~14:00</p></Column>
+		<Column><p>Go to the Gaepo cluster</p></Column>
+	</Row>
+	<Row>
+		<Column><p>14:00~14:30</p></Column>
+		<Column><p>42Seoul cluster tour</p></Column>
+	</Row>
+	<Row>
+		<Column><p>14:30~15:00</p></Column>
+		<Column><p>Orientation & Publish the hackathon challenges</p></Column>
+	</Row>
+	<Row>
+		<Column><p>15:00~</p></Column>
+		<Column><p>2<sup>nd</sup> hackathon</p></Column>
+	</Row>
+</Grid>
+<br/>
+<h3>Day 5</h3>
+<br/>
+<Grid>
+	<Row>
+		<Column><p>~15:00</p></Column>
+		<Column><p>2<sup>nd</sup> hackathon</p></Column>
+	</Row>
+	<Row>
+		<Column><p>15:00~17:00</p></Column>
+		<Column><p>Hackathon presentation</p></Column>
+	</Row>
+	<Row>
+		<Column><p>17:00~17:30</p></Column>
+		<Column><p>Judging process</p></Column>
+	</Row>
+	<Row>
+		<Column><p>17:30~18:00</p></Column>
+		<Column><p>Awards ceremony & Closing remarks</p></Column>
+	</Row>
+	<Row>
+		<Column><p>18:00~</p></Column>
+		<Column><p>Group photo & Dinner</p></Column>
+	</Row>
+</Grid>
+
