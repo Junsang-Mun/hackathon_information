65580475731af1988c7efa28b1ed435c18880764
Junsang Mun <mun.js@aol.com>
2024-09-21 07:40:33 +0900

    feat: weather page update

--- src/routes/weather/+page.svelte
+++ src/routes/weather/+page.svelte
@@ -1,1 +1,29 @@
+<script>
+	import { onMount } from 'svelte';
+	let data = null;
+	let error = null;
+
+	onMount(async () => {
+		try {
+			const response = await fetch('/api/weather');
+			if (!response.ok) {
+				throw new Error('Failed to fetch');
+			}
+			data = await response.json();
+		} catch (err) {
+			error = err.message;
+		}
+	});
+</script>
+
 <h1>Weather</h1>
+
+{#if error}
+	<p>Error: {error}</p>
+{:else if !data}
+	<p>Loading...</p>
+{:else}
+	<div>
+	<pre>{JSON.stringify(data, null, 2)}</pre>
+	</div>
+{/if}
