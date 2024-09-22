fc42db926c155152f98619527955b55bfd059c00
Junsang Mun <mun.js@aol.com>
2024-09-22 20:09:06 +0900

    feat: add cluster information

--- src/routes/cluster/gaepo/+page.svelte
+++ src/routes/cluster/gaepo/+page.svelte
@@ -1,1 +1,5 @@
 <h1>Gaepo Cluster</h1>
+<hr/>
+<h3>Location</h3>
+<p>KO: 서울 강남구 개포로 416 새롬관</p>
+<p>EN: Sae-rom building, 416, Gaepo-ro, Gangnam-gu, Seoul-si</p>
--- src/routes/cluster/seocho/+page.svelte
+++ src/routes/cluster/seocho/+page.svelte
@@ -1,1 +1,5 @@
 <h1>Seocho Cluster</h1>
+<hr/>
+<h3>Location</h3>
+<p>KO: 서울 서초구 강남대로 327 4층</p>
+<p>EN: 4th floor, 327, Gangnam-daero, Seocho-gu, Seoul-si</p>
