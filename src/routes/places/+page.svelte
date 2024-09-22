9537e01bea3c9b8bd6c9dee6f53c287ee96b4fd7
Junsang Mun <mun.js@aol.com>
2024-09-22 20:12:12 +0900

    feat: update places to visit

--- src/routes/places/+page.svelte
+++ src/routes/places/+page.svelte
@@ -1,1 +1,44 @@
 <h1>Places to Visit</h1>
+<hr/>
+<h2>Gwanghwamun</h2>
+<p>Gwanghwamun (Korean: 광화문; Hanja: 光化門) is the main and largest gate of Gyeongbok Palace, in Jongno-gu, Seoul, South Korea. It is located at a three-way intersection at the northern end of Sejongno. As a landmark and symbol of Seoul's long history as the capital city during the Joseon period, the gate has gone through multiple periods of destruction and disrepair. The most recent large-scale restoration work on the gate was finished and it was opened to the public on August 15, 2010.</p>
+<a href="https://en.wikipedia.org/wiki/Gwanghwamun" target="_blank">Wikipedia</a>
+<br/>
+<h2>Gyeongbokgung</h2>
+<p>Gyeongbokgung (Korean: 경복궁; Hanja: 景福宮; lit. Blessing Scenery Palace), also known as Gyeongbokgung Palace, was the main royal palace of the Joseon dynasty. Built in 1395, it is located in northern Seoul, South Korea. The largest of the Five Grand Palaces built by the Joseon dynasty, Gyeongbokgung served as the home of the royal family and the seat of government.</p>
+<a href="https://en.wikipedia.org/wiki/Gyeongbokgung" target="_blank">Wikipedia</a>
+<br/>
+<h2>Gwanghwamun Square</h2>
+<p>Gwanghwamun Square(also known as Gwanghwamun Plaza; Korean: 광화문광장) is a public square on Sejongno, Jongno-gu, Seoul, South Korea. Serving as a public space and at times road for centuries of Korean history, it is also historically significant as the location of royal administrative buildings, known as Yukjo-geori or Street of Six Ministries; and features statues of Admiral Yi Sun-sin of Joseon Dynasty and King Sejong the Great of Joseon.</p>
+<a href="https://en.wikipedia.org/wiki/Gwanghwamun_Square" target="_blank">Wikipedia</a>
+<br/>
+<h2>Bukchon Hanok Village</h2>
+<p>Bukchon Hanok Village (Korean: 북촌한옥마을) is a residential neighborhood in Jongno District, Seoul, South Korea. It has many restored traditional Korean houses, called hanok. This has made it a popular tourist destination, which has caused some friction with the residents who live there.</p>
+<a href="https://en.wikipedia.org/wiki/Bukchon_Hanok_Village" target="_blank">Wikipedia</a>
+<br/>
+<h2>Cheonggyecheon</h2>
+<p>Cheonggyecheon (Korean: 청계천) is a 10.9-kilometre-long (6.8 mi) stream and public space in downtown Seoul, South Korea. A natural stream sourced from the Suseongdong Valley in Inwangsan, it was historically maintained as part of Seoul's early sewerage until the mid-20th century, when post-Korean War rapid economic development and deteriorating conditions prompted the filling of the stream with concrete and the construction of an elevated freeway, the Cheonggye Expressway, in its place. In 2003, the city government began an urban renewal project to disassemble the expressway and restore the stream, which was completed in 2005 at a cost of over ₩386 billion (approximately US$281 million).</p>
+<a href="https://en.wikipedia.org/wiki/Cheonggyecheon" target="_blank">Wikipedia</a>
+<br/>
+<h2>Bosingak</h2>
+<p>Bosingak (Korean: 보신각) is a large bell pavilion on Jongno in Seoul, South Korea. The bell in Bosingak gives Jongno its name, which translates to "bell street". The pavilion was originally constructed in 1395 but destroyed many times by both war and fire. It was designated Bosingak by Emperor Gojong in 1895. The bell was cast in 1468.</p>
+<a href="https://en.wikipedia.org/wiki/Cheonggyecheon" target="_blank">Wikipedia</a>
+<br/>
+<h2>Myeong-dong</h2>
+<p>Myeong-dong (Korean: 명동; Hanja: 明洞; lit. 'bright neighborhood') is a dong (neighborhood) in Jung District, Seoul, South Korea between Chungmu-ro, Eulji-ro, and Namdaemun-ro. Myeongdong is known for being one of Seoul's main shopping, parade route, and tourism districts. In 2023, it was listed as the ninth most expensive shopping street in the world. The area is known for its two historically significant sites, namely the Myeongdong Cathedral and the Myeongdong Theater. Performances at Myeongdong Nanta Theater are popular.</p>
+<a href="https://en.wikipedia.org/wiki/Myeong-dong" target="_blank">Wikipedia</a>
+<br/>
+<h2>Dongdaemun Design Plaza</h2>
+<p>Dongdaemun Design Plaza (DDP; Korean: 동대문 디자인 플라자) is a major urban development landmark in Seoul, South Korea, designed by Zaha Hadid and Samoo, with a distinctively neofuturistic design characterized by the "powerful, curving forms of elongated structures." The landmark is the centerpiece of South Korea's fashion hub and popular tourist destination, Dongdaemun, featuring a walkable park on its roofs, large global exhibition spaces, futuristic retail stores, and restored parts of the Seoul fortress.</p>
+<a href="https://en.wikipedia.org/wiki/Dongdaemun_Design_Plaza" target="_blank">Wikipedia</a>
+<br/>
+<h2>Hangang Park</h2>
+<p>Hangang Park (Korean: 한강공원) is a park that consists of 12 parks in Seoul, South Korea. It consists of Gwangnaru Park, Jamsil Park, Ttukseom Park, Jamwon Park, Ichon Park, Banpo Park, Mangwon Park, Yeouido Park, Nanji Park, Gangseo Park, Yanghwa Park, and Seonyudo Park. In Hangang Park, there is an abundance of sporting facilities such as soccer fields, skateboarding and in-line skating parks, tennis courts, hiking/biking trails, etc. There are also many swimming pools and many water-related sports such as water-skiing, yachting, boat racing, and fishing.</p>
+<a href="https://en.wikipedia.org/wiki/Hangang_Park" target="_blank">Wikipedia</a>
+<br/>
+
+<style>
+	h2 {
+		padding-top: 1rem;
+	}
+</style>
